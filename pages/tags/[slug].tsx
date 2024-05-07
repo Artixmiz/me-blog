1878729679b7cf3614e5fedfc8df232a69ca11b3
Artixmiz <57831268+Artixmiz@users.noreply.github.com>
2024-05-07 10:19:48 +0700

    Update [slug].tsx

--- pages/tags/[slug].tsx
+++ pages/tags/[slug].tsx
@@ -53,7 +53,7 @@ function Tags({ tagPosts }: { tagPosts: string }) {
       <div className="px-4 mx-auto max-w-screen-xl">
 
         <h2 className="mb-8 text-2xl font-bold text-gray-900 dark:text-white">
-          More articles from {tagName.replaceAll("-", " ")}
+         testtesttesttest More articles from {tagName.replaceAll("-", " ")}
         </h2>
 
         <div className="container my-12 mx-auto grid grid-cols-1 gap-12 md:gap-12 lg:gap-12 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4 ">
